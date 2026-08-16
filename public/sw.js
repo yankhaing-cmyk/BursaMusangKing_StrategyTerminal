89d9ca8fe09fcea7843ae63b48fc80e381ba89a0
yankhaing-cmyk <yankhaing@gmail.com>
2026-08-16 13:53:58 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const C='bursa-strategy-terminal-v2';
+const C='bursa-strategy-terminal-v3';
 const CORE=['/','/app.css','/app.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',e=>{
